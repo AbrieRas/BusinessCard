0ddef560749fd0a65e071a95a2c4d4b4dec7dbb9
Computer <www.headoff.com@gmail.com>
2024-06-07 09:30:46 +0200

    Complete Summary section and styles. Add Summary font-family styles to app.css

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,9 +2,20 @@ import "./../styles/summary.css";
 
 const Summary = () => {
     return (
-        <>
-            <h1>Summary goes here</h1>
-        </>
+        <div className="summary-container">
+            <h2>About</h2>
+            <p>
+                As a passionate student pursuing the Certified Ethical Hacker
+                (CEH) certification through EC-Council, my motivation springs
+                from a significant interest in technology and an unyielding
+                commitment to staying well-versed of cutting-edge advancements.
+            </p>
+            <h2>Interests</h2>
+            <p>
+                Diving into capture the flag hacking/cyber forensics tournaments
+                for efficient and scalable security solutions.
+            </p>
+        </div>
     );
 };
 
